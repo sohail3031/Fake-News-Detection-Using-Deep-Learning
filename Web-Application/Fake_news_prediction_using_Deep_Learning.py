6cfc735a5257c59298e332a019aa1238fdeca0a7
sohail3031 <mohdsohailahmed46@gmail.com>
2024-12-14 21:06:54 -0500

    Add Requirements File

--- Web-Application/Fake_news_prediction_using_Deep_Learning.py
+++ Web-Application/Fake_news_prediction_using_Deep_Learning.py
@@ -15,27 +15,48 @@ from keras_preprocessing.sequence import pad_sequences
 from keras.models import load_model
 
 # downloading necessary packages
-# nltk.download("stopwords", quiet=True)
-# nltk.download("punkt", quiet=True)
-# nltk.download("wordnet", quiet=True)
-# nltk.download('punkt_tab', quiet=True)
+nltk.download("stopwords", quiet=True)
+nltk.download("punkt", quiet=True)
+nltk.download("wordnet", quiet=True)
+nltk.download('punkt_tab', quiet=True)
 
 # creating stemmer for stemming the text
 stemmer = PorterStemmer()
 
+# uploading files
+lstm_model_upload = st.file_uploader("Choose LSTM Model", type=["h5"])
+cnn_model_upload = st.file_uploader("Choose CNN Model", type=["h5"])
+cobra_model_upload = st.file_uploader("Choose COBRA Model", type=["h5"])
+lstm_token_upload = st.file_uploader("Choose LSTM Tokens", type=["pkl"])
+cnn_token_upload = st.file_uploader("Choose CNN Tokens", type=["pkl"])
+cobra_token_upload = st.file_uploader("Choose COBRA Tokens", type=["pkl"])
+
 # loading models
-lstm_model = load_model(os.path.abspath("lstm_model.h5"))
-cnn_model = load_model(os.path.abspath("cnn_model.h5"))
-cobra_model = load_model(os.path.abspath("cobra_model.h5"))
+# lstm_model = load_model(os.path.abspath("models/lstm_model.h5"))
+# cnn_model = load_model(os.path.abspath("models/cnn_model.h5"))
+# cobra_model = load_model(os.path.abspath("models/cobra_model.h5"))
+
+lstm_model = load_model(lstm_model_upload)
+cnn_model = load_model(cnn_model_upload)
+cobra_model = load_model(cobra_model_upload)
 
 # loading tokenizer of each model
-with open(os.path.abspath("lstm_tokenizer.pkl"), "rb") as handle:
+# with open(os.path.abspath("models/lstm_tokenizer.pkl"), "rb") as handle:
+#     lstm_tokenizer = pickle.load(handle)
+#
+# with open(os.path.abspath("models/cnn_tokenizer.pkl"), "rb") as handle:
+#     cnn_tokenizer = pickle.load(handle)
+#
+# with open(os.path.abspath("models/cobra_tokenizer.pkl"), "rb") as handle:
+#     cobra_tokenizer = pickle.load(handle)
+
+with open(lstm_token_upload, "rb") as handle:
     lstm_tokenizer = pickle.load(handle)
 
-with open(os.path.abspath("cnn_tokenizer.pkl"), "rb") as handle:
+with open(cnn_token_upload, "rb") as handle:
     cnn_tokenizer = pickle.load(handle)
 
-with open(os.path.abspath("cobra_tokenizer.pkl"), "rb") as handle:
+with open(cobra_token_upload, "rb") as handle:
     cobra_tokenizer = pickle.load(handle)
 
 
